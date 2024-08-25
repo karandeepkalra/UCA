12d4a8c2231492e9146f3d6f6d6dc4ec38e400af
karandeepkalra <karandeepkaur0089@gmail.com>
2024-08-25 18:32:39 +0530

    Made a simple static library of simple calculator just to add,sub,mul,div purpose

---
+++ Rohit_sir/code.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include "cal.h"
+void main()
+{
+	add(2,3);
+	mul(2,3);
+	div(2,3);
+	sub(3,2);
+        
+}
---
+++ Rohit_sir/lib_add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int add(int a,int b)
+{
+         printf("%d",a+b);
+	 return 0;
+}
+int sub(int a,int b)
+{
+	printf("%d",a-b);
+}
+int mul(int a,int b)
+{
+	printf("%d",a*b);
+}
+int div(int a,int b)
+{
+	printf("%d",a/b);
+}
